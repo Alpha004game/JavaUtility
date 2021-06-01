38cb110272354aea2e33f44e84932467597d1798
Alpha004game <alpha004thegamer@gmail.com>
2021-06-01 11:13:52 +0200

    mod

---
+++ src/Sort/QuickSort.java
@@ -0,0 +1,57 @@
+package Sort;
+
+public class QuickSort<T extends Comparable<T>>
+{
+    //Instanzia per metodi con compareTo
+
+    private int partition(T array[], int low, int high) {
+
+        // choose the rightmost element as pivot
+        T pivot = array[high];
+
+        // pointer for greater element
+        int i = (low - 1);
+
+        // traverse through all elements
+        // compare each element with pivot
+        for (int j = low; j < high; j++) {
+            if (array[j].compareTo(pivot) <= 0) {
+
+                // if element smaller than pivot is found
+                // swap it with the greater element pointed by i
+                i++;
+
+                // swapping element at i with element at j
+                T temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+
+        }
+        // find pivot element such that
+        // elements smaller than pivot are on the left
+        // elements greater than pivot are on the right
+        T temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+
+        // return the position from where partition is done
+        return (i + 1);
+    }
+
+    public void quickSort(T array[], int low, int high) {
+        if (low < high) {
+
+            // find pivot element such that
+            // elements smaller than pivot are on the left
+            // elements greater than pivot are on the right
+            int pi = partition(array, low, high);
+
+            // recursive call on the left of pivot
+            quickSort(array, low, pi - 1);
+
+            // recursive call on the right of pivot
+            quickSort(array, pi + 1, high);
+        }
+    }
+}
--- src/personale/Main.java
+++ src/personale/Main.java
@@ -4,6 +4,7 @@ package personale;
 import Liste.Lista;
 import Liste.Lista2;
 import Liste.Nodo;
+import Sort.QuickSort;
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -62,7 +63,7 @@ public class Main {
         {
             System.out.println("Errore");
         }*/
-
+        /*
         Lista<Persona> lista=new Lista<>();
 
         lista.inserisciInCoda(new Persona("A", "A", 1));
@@ -78,7 +79,31 @@ public class Main {
         System.out.println(tmp.getInfo().toString()+"\n");
         //System.out.println("aaaa");
         System.out.println(lista.visita());
+        */
 
-
+        QuickSort<Persona> sort=new QuickSort<>();
+        Persona[] array=new Persona[10];
+        //3,1,4,2,5,7,8,6,9,0
+        array[0]=new Persona("A","A", 3);
+        array[1]=new Persona("A","A", 1);
+        array[2]=new Persona("A","A", 4);
+        array[3]=new Persona("A","A", 2);
+        array[4]=new Persona("A","A", 5);
+        array[5]=new Persona("A","A", 7);
+        array[6]=new Persona("A","A", 8);
+        array[7]=new Persona("A","A", 6);
+        array[8]=new Persona("A","A", 9);
+        array[9]=new Persona("A","A", 0);
+        System.out.println("Pre sort...");
+        for(int i=0; i< array.length; i++)
+        {
+            System.out.println(array[i].toString());
+        }
+        sort.quickSort(array, 0, array.length-1);
+        System.out.println("After sort...");
+        for(int i=0; i< array.length; i++)
+        {
+            System.out.println(array[i].toString());
+        }
     }
 }
